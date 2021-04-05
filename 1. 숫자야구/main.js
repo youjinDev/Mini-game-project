a1f78dde61a474723daeddc9972560aac7f4521e
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-04-05 14:57:43 +0900

    숫자야구 : final commit✔

--- 1. 숫자야구/main.js
+++ 1. 숫자야구/main.js
@@ -5,86 +5,91 @@ const inputNumber = document.querySelector('.number_input');
 const submitBtn = document.querySelector('.submit');
 const answerBox = document.querySelector('.answer>p');
 const gameChance = document.querySelector('.chance');
+const gameImg = document.querySelector('.main_img');
 
-let parsedClientAnswerArray;
-let chance = GMAE_CHANCE;
+let clientAnswerArray;
 let answerArray;
+let chance;
 
-setAnswerArray();
+// Setting answer number and game initialization
+initGame();
 
-submitBtn.addEventListener('click', () => {    
-    if (inputNumber.value > 9999 || inputNumber.value < 1000 || inputNumber.value == '') {
+// onClick main game img
+gameImg.addEventListener('click', initGame);
+
+// onClick button
+submitBtn.addEventListener('click', () => {
+    let inputNumberValue = inputNumber.value.split('');
+    clientAnswerArray = inputNumberValue.map((element) => parseInt(element, 10));
+    if (clientAnswerArray.length !== 4) {
         alert('올바른 숫자를 입력하세요.');
         return;
     }
-    let clientAnswerArray = inputNumber.value.split('', 4);
-    parsedClientAnswerArray = clientAnswerArray.map((element) => parseInt(element, 10));
-    console.log(answerArray);
-
-    // answerArray와 clientArray의 숫자를 비교
-    showAnswer();
-    countChance();
 
     // 맞추면
-    if (isEqual(parsedClientAnswerArray, answerArray)) {
+    if (isEqual(clientAnswerArray, answerArray)) {
         alertWin();
         initGame();
-        setAnswerArray();
+        return;
     }
 
-    function showAnswer() {
-        let strikeNum = 0;
-        let ballNum = 0;
-
-        for (let i = 0; i < answerArray.length ; i++) {
-            if (answerArray[i] == parsedClientAnswerArray[i]) {
-                strikeNum++;
-            }
-            if(parsedClientAnswerArray.includes(answerArray[i])) {
-                ballNum++;
-            }
-        }
-        answerBox.innerHTML = `<strong>${strikeNum}</strong> strike! <br> <strong>${ballNum}</strong> ball!`;
-    }
+    // answerArray와 clientArray의 숫자를 비교
+    showAnswer(answerArray, clientAnswerArray);
+    countChance();
 });
 
-function isEqual(parsedClientAnswerArray, answerArray) {
-    if (parsedClientAnswerArray.join('') == answerArray.join('')) {
-        return true;
-    }
-}
-
-function alertWin() {
-    alert(`HOERUN! The answer is ${answerArray.join('')}!`);
-}
+// function
 
 function initGame() {
+    setAnswerArray();
     chance = GMAE_CHANCE;
     gameChance.innerHTML = chance;
     inputNumber.value = '';
     answerBox.innerHTML = `<p>각 자릿수가 중복되지 않는 <br> 네 자리 숫자를 입력하세요</p>`;
 }
 
+function setAnswerArray() {
+    let candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    answerArray = [];
+    for (let i = 0; i < 4 ; i++) {
+        let index = Math.floor(Math.random() * 9);
+        let selected = candidates.splice((index - i), 1)[0];
+        answerArray.push(selected);
+    }
+    console.log(answerArray);
+}
+
+function isEqual(arr1, arr2) {
+    if (arr1.join('') == arr2.join('')) return true;
+}
+
+function showAnswer(arr1, arr2) {
+    let strikeNum = 0;
+    let ballNum = 0;
+
+    for (let i = 0; i < arr1.length ; i++) {
+        if (arr1[i] == arr2[i]) {
+            strikeNum++;
+        }
+        if(arr2.includes(arr1[i])) {
+            ballNum++;
+        }
+    }
+    answerBox.innerHTML = `<strong>${strikeNum}</strong> strike! <br> <strong>${ballNum}</strong> ball!`;
+}
+
+function alertWin() {
+    alert(`HOMERUN! The answer is ${answerArray.join('')}!`);
+}
+
 function countChance() {
+    chance--;
+
     // 기회 다 쓰면
     if (chance === 0) {
         alert('GAME OVER!');
         initGame();
-        setAnswerArray();
         return;
     }
-    --chance;
     gameChance.innerHTML = chance;
-}
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * 9) + 1;
-}
-
-function setAnswerArray() {
-    answerArray = [];
-    answerArray.push(getRandomNumber());
-    answerArray.push(getRandomNumber());
-    answerArray.push(getRandomNumber());
-    answerArray.push(getRandomNumber());
 }
