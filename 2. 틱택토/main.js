eaf4c795ddd9a59064ad173d4bdc24e4eddaeb81
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-04-06 15:35:19 +0900

    틱택토 : final commit✔

--- 2. 틱택토/main.js
+++ 2. 틱택토/main.js
@@ -5,35 +5,47 @@ const buttonX = document.querySelector('.btn__x');
 const buttonO = document.querySelector('.btn__o');
 
 let mainArray = [];
+// 스타트 기본값 - x 선공
 let playerX = true;
-let isStarted;
-let countX = 4;
+let startPlayer = 'x';
+let countX = 5;
 let countO = 4;
 
 // mainArray에 이미 만들어진 테이블 객체를 가져와야 함
-makeArray();
-initGame();
+makeMainArray();
+
+buttonX.addEventListener('click', () => {
+    // X가 선공, 기본값
+    buttonDisabled(true);
+});
+
+buttonO.addEventListener('click', () => {
+    // O가 선공
+    playerX = false;
+    startPlayer = 'o';
+    setCount();
+    buttonDisabled(true);
+})
 
 mainTable.addEventListener('click', (e) => {
+    buttonDisabled(true);
     const target = e.target;
+    // target === TD 일 때 한정
     if (target.tagName != 'TD') {
-        console.log('잘못 누름');
+        console.log('It is not a right event target');
         return;
     }
 
     let isEmpty = (target.innerText !== '❌') && (target.innerText !== '⭕');
-    // target === TD 일 때 한정
-    // 칸 클릭 시 비었으면 클릭한 칸에 X를 넣는다
     if (!isEmpty) {
         alert('Already occupied!');
         return;
     } else {
         putStone(target);
-        console.log(countX, countO);
-    }
+        isWin(target);    }
 });
 
-function makeArray() {
+function makeMainArray() {
     for (let i = 1; i <= 3 ; i++) {
         let row = [];
         for (let j = 1 ; j <= 3; j++) {
@@ -42,10 +54,10 @@ function makeArray() {
         }
         mainArray.push(row);
     }
-    console.log(mainArray);
 }
 
 function putStone(target) {
+    console.log('putStone 실행');
     if(playerX) {
         target.innerText = '❌'
         target.style.backgroundColor = '#ffff00';
@@ -67,38 +79,111 @@ function setCount() {
     }
 }
 
-function setPlayer() {
+function isWin(target) {
+    // o or x가 세 개 이상 놓일 때부터 함수 실행
+    if (countO > 2 || countX > 2) {
+        return;
+    } else {
+        console.log (countX, countO, '지금부터 isWin 실행');
 
+        // 가로 빙고
+        rowCheck(target);
+    
+        // 세로 빙고
+        columnCheck(target);
+    
+        // 대각선 빙고
+        diagonalCheck(target);
+    }
+
+    // 무승부 : countX, countO이 둘 다 0일 때
+    drawCheck(); 
 }
 
-function initGame() {
-    setCount();
-    // td의 innerText 토끼로 바꾸기
-    // OX버튼 disabled true로 바꾸기
-    // counter 초기화하기
-}
-
-function isWin() {
-    // o or x가 세 개 이상일 때부터
+function rowCheck(target) {
     // 가로로 이기기
     // innerText=x가 mainArray[i][j]에서 i가 0, 1, 2
-    // 세로로 이기기
-    // innerText=x가 mainArray[i][j]에서 j가 0, 1, 2
-    // 대각선으로 이기기
-    // innerText=x가 mainArray[i][j]에서 j가 같은지 확인
-    // mainArray[0][0], mainArray[1][1], mainArray[2][2]
-    // mainArray[0][2], mainArray[1][1], mainArray[0][1]
-    // 무승부 : countX, countO이 둘 다 0일 때
+    for (let i = 0; i < 3 ; i++) {
+        let tempArr = [];
+        for (let j = 0 ; j < 3 ; j++) {
+            tempArr.push(mainArray[i][j].innerText);
+        }
+        if (tempArr.every(element => element === '❌')) {
+            gameOver('win', target.innerText);
+            return;
+        } else if (tempArr.every(element => element === '⭕')) {
+            gameOver('win', target.innerText);
+            return;
+        }
+        console.log(`tempArrRow is ${tempArr}`);
+    }
+}
+
+function columnCheck(target) {
+    for (let i = 0 ; i < 3 ; i++) {
+        let tempArr = [];
+        for (let j = 0; j < 3 ; j++) {
+            tempArr.push(mainArray[j][i].innerText);
+        }
+        if (tempArr.every(element => element === '❌')) {
+            gameOver('win', target.innerText);
+            return;
+        } else if (tempArr.every(element => element === '⭕')) {
+            gameOver('win', target.innerText);
+            return;
+        }
+        console.log(`tempArrColumn is ${tempArr}`);
+    }
+}
+
+function diagonalCheck(target) {
+    console.log('대각선 check start');
+
+    let tempArray = [];
+    tempArray.push(mainArray[0][2].innerText);
+    tempArray.push(mainArray[1][1].innerText);
+    tempArray.push(mainArray[2][0].innerText);
+    let isRightToLeftDiagonal = tempArray.every(element => element == tempArray[0]);
+
+    tempArray.length = 0;
+    console.log(tempArray);
+    tempArray.push(mainArray[0][0].innerText);
+    tempArray.push(mainArray[1][1].innerText);
+    tempArray.push(mainArray[2][2].innerText);
+    console.log(tempArray);
+
+    let isLeftTorightDiagonal = tempArray.every(element => element == tempArray[0]);
+
+    if (isRightToLeftDiagonal || isLeftTorightDiagonal) {
+        gameOver('win', target.innerText);
+    }
+        return;
+}
+
+function drawCheck() {
+    // 선공쪽 count == 0일 때 draw
+    if (startPlayer == 'x' && countX == 0) {
+        gameOver('draw');
+        return;
+    } else if(startPlayer == 'o' && countO == 0) {
+        gameOver('draw');
+        return;
+    }
 }
 
 function gameOver(reason, player) {
     switch (reason) {
         case 'win':
-            alert(`${player}가 이겼습니다!`);
+            alert(`Player ${player}가 이겼습니다!`);
             break;
         case 'draw':
             alert('비겼습니다!');
             break;
     }
-    initGame();
+    buttonDisabled(false);
+}
+
+function buttonDisabled(boolean) {
+    buttonO.disabled = boolean;
+    buttonX.disabled = boolean;
 }
